28de2b3d74dee5f972d901bf547b3d6478db8a2f
Alexander Michels <alexandercm4297@gmail.com>
2018-05-15 19:01:11 -0400

    have the APIKey reading and decoding the JSON object

--- APIKey.py
+++ APIKey.py
@@ -1,9 +1,17 @@
-'''
-Simple class to grab a WeatherUnderground API key from a file so I don't have to put that in Git
-'''
 import urllib.request, json
 
 class APIKey:
+    '''
+    Simple class to interact with the WeatherUnderground API 
+    
+    ---Instance Variables
+    key = a string containing the API key
+    
+    ---Methods
+    __init__(filename) = takes a string, opens said file, and sets the key field as the read().strip() output
+    __str__ = returns the key field
+    get_weather_url(state, city, key)
+    '''
     key = ""
     def __init__(self, filename):
         file = open(filename)
@@ -12,15 +20,12 @@ class APIKey:
     def __str__(self):
         return self.key
         
-    def get_weather_url(state, city, key):
-        return "http://api.wunderground.com/api/{}/conditions/q/{}/{}.json".format(key, state, city)
+    def get_weather(self, state, city):
+        return urllib.request.urlopen("http://api.wunderground.com/api/{}/conditions/q/{}/{}.json".format(self.key, state, city)).read().decode("utf-8")
 
 def main():
     key = APIKey('WeatherUndergroundAPIKey')
-    print(key)
-    url = key.get_weather_url('PA', 'New_Wilmington')
-    print(url)
-    res = urllib.request.urlopen(url)
+    res = key.get_weather('PA', 'New_Wilmington')
     print(res)
     
 if __name__ == "__main__" :
