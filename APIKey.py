4dad1107952509295f8e4b8b3b79b742bbd2abcd
Alexander Michels <alexandercm4297@gmail.com>
2018-05-15 20:09:13 -0400

    Working on reading conditions

--- APIKey.py
+++ APIKey.py
@@ -1,4 +1,5 @@
-import urllib.request, json
+import urllib.request, json, vlc
+from gtts import gTTS
 
 class APIKey:
     '''
@@ -10,9 +11,10 @@ class APIKey:
     ---Methods
     __init__(filename) = takes a string, opens said file, and sets the key field as the read().strip() output
     __str__ = returns the key field
-    get_weather_url(state, city, key)
+    get_weather(state, city) = returns the JSON of the weather at the location
     '''
     key = ""
+    curr_json = ""
     def __init__(self, filename):
         file = open(filename)
         self.key = file.read().strip()
@@ -21,12 +23,34 @@ class APIKey:
         return self.key
         
     def get_weather(self, state, city):
-        return urllib.request.urlopen("http://api.wunderground.com/api/{}/conditions/q/{}/{}.json".format(self.key, state, city)).read().decode("utf-8")
+        self.curr_json = json.loads(urllib.request.urlopen("http://api.wunderground.com/api/{}/conditions/q/{}/{}.json".format(self.key, state, city)).read().decode("utf-8"))
+        return self.curr_json
+    
+    def print_json(self):
+        print(json.dumps(self.curr_json, sort_keys = True, indent = 4))
 
+    def get_curr_temp(self):
+        return self.curr_json["current_observation"]['temp_f']       
+        
+    def get_curr_weather(self):
+        return self.curr_json["current_observation"]['weather']
+        
+    def get_curr_feels_like(self):
+        return self.curr_json["current_observation"]["feelslike_f"]
+        
+    def get_report(self):
+        return "It is currently {} degrees farenheit, but feels like {}, and it is {} out today".format(self.get_curr_temp(), self.get_curr_feels_like(), self.get_curr_weather())
+        
+        
 def main():
     key = APIKey('WeatherUndergroundAPIKey')
     res = key.get_weather('PA', 'New_Wilmington')
-    print(res)
+    key.print_json()
+    print(key.get_report())
+    tts = gTTS(text=key.get_report(), lang='en')
+    tts.save("weather.mp3")
+    p = vlc.MediaPlayer("weather.mp3")
+    p.play()
     
 if __name__ == "__main__" :
     main()
