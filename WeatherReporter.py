632bbd9395f94c458d52a488417a9affd25abb75
Alexander Michels <alexandercm4297@gmail.com>
2018-05-23 21:40:54 -0400

    Converted to Python 2 to work with Google Cloud

--- WeatherReporter.py
+++ WeatherReporter.py
@@ -1,7 +1,11 @@
-import urllib.request, json, vlc, subprocess, os, sys
+#!/usr/bin/env python
+from __future__ import absolute_import
+import json, vlc, urllib2, urllib
+from io import open
+from google.cloud import texttospeech
 
-class WeatherReporter:
-    '''
+class WeatherReporter(object):
+    u'''
     Simple class to interact with the WeatherUnderground API 
     
     ---Instance Variables
@@ -31,10 +35,10 @@ class WeatherReporter:
     read_mp3() = plays the MP3 in the mp3_filename field
     '''
 
-    curr_json = ""
-    fore_json = ""
-    key = ""
-    mp3_filename = "weather.mp3"
+    curr_json = u""
+    fore_json = u""
+    key = u""
+    mp3_filename = u"weather.mp3"
     def __init__(self, filename, state, city):
         file = open(filename)
         self.key = file.read().strip()
@@ -45,84 +49,89 @@ class WeatherReporter:
         return self.key
     
     def format_string(self, string):
-        string = string.replace("F.", ".")
-        dirAbr = [ ' N ', ' E ', ' S ', ' W ', ' NW ', ' NE ', ' SW ', ' SE ', ' NNE ', ' ENE ', ' ESE ', ' SSE ', ' SSW ', ' WSW ', ' WNW ', ' NNW ']
-        dirStr = [ ' North ', ' East ', ' South ', ' West ', ' Northwest ', ' Northeast ', ' Southwest ', ' Southeast ', ' North Northeast ', ' East Northeast ', ' East Southeast ', ' South Southeast ', ' South Southwest ', ' West Southwest ', ' West Northwest ', ' North Northwest ']
-        for i in range(0, len(dirAbr)):
+        string = string.replace(u"F.", u".")
+        dirAbr = [ u' N ', u' E ', u' S ', u' W ', u' NW ', u' NE ', u' SW ', u' SE ', u' NNE ', u' ENE ', u' ESE ', u' SSE ', u' SSW ', u' WSW ', u' WNW ', u' NNW ']
+        dirStr = [ u' North ', u' East ', u' South ', u' West ', u' Northwest ', u' Northeast ', u' Southwest ', u' Southeast ', u' North Northeast ', u' East Northeast ', u' East Southeast ', u' South Southeast ', u' South Southwest ', u' West Southwest ', u' West Northwest ', u' North Northwest ']
+        for i in xrange(0, len(dirAbr)):
             string = string.replace(dirAbr[i], dirStr[i])
         return string
         
     def get_conditions(self, state, city):
-        self.curr_json = json.loads(urllib.request.urlopen("http://api.wunderground.com/api/{}/conditions/q/{}/{}.json".format(self.key, state, city)).read().decode("utf-8"))
+        self.curr_json = json.loads(urllib2.urlopen(u"http://api.wunderground.com/api/{}/conditions/q/{}/{}.json".format(self.key, state, city)).read().decode(u"utf-8"))
         return self.curr_json
             
     def get_report_mp3(self):
         #tts = gTTS(text = self.get_report_string(), lang='en')
         #tts.save(self.mp3_filename)
-        path = os.path.dirname(os.path.realpath(__file__))
-        python3_command = "{}/GoogleCloudTextToSpeech.py weather.txt".format(path)
-        process = subprocess.Popen(python3_command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
+        client = texttospeech.TextToSpeechClient()
+    
+        input_text = texttospeech.types.SynthesisInput(text=self.get_report_string())
+        
+        voice = texttospeech.types.VoiceSelectionParams(language_code='en-US', ssml_gender=texttospeech.enums.SsmlVoiceGender.MALE)
+        audio_config = texttospeech.types.AudioConfig(audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+        response = client.synthesize_speech(input_text, voice, audio_config)
+    
+        with open('weather.mp3', 'wb') as out:
+            out.write(response.audio_content)
         
     def get_conditions_string(self):
         curr_temp = self.get_curr_temp()
         feels_like = self.get_curr_feels_like()
         if (abs(curr_temp - feels_like) > 5):
-            return "It is currently {} degrees farenheit, but feels like {}, and it is {} out.".format(curr_temp, feels_like, self.get_curr_weather())
+            return u"Wilmington area weather, it is currently {} degrees farenheit, but feels like {}, and it is {} out.".format(curr_temp, feels_like, self.get_curr_weather())
         else:
-            return "It is currently {} degrees farenheit and it is {} out.".format(curr_temp, self.get_curr_weather())
+            return u"Wilmington area weather, it is currently {} degrees farenheit and it is {} out.".format(curr_temp, self.get_curr_weather())
         
     def get_curr_feels_like(self):
-        return float(self.curr_json["current_observation"]["feelslike_f"])
+        return float(self.curr_json[u"current_observation"][u"feelslike_f"])
 
     def get_curr_temp(self):
-        return float(self.curr_json["current_observation"]['temp_f'])    
+        return float(self.curr_json[u"current_observation"][u'temp_f'])    
         
     def get_curr_weather(self):
-        return self.curr_json["current_observation"]['weather']
+        return self.curr_json[u"current_observation"][u'weather']
         
     def get_forecast(self, state, city):
-        self.fore_json = json.loads(urllib.request.urlopen("http://api.wunderground.com/api/{}/forecast/q/{}/{}.json".format(self.key, state, city)).read().decode("utf-8"))
+        self.fore_json = json.loads(urllib2.urlopen(u"http://api.wunderground.com/api/{}/forecast/q/{}/{}.json".format(self.key, state, city)).read().decode(u"utf-8"))
         return self.curr_json
     
     def get_forecast_next_step(self):
-        return self.fore_json["forecast"]["txt_forecast"]["forecastday"][1]
+        return self.fore_json[u"forecast"][u"txt_forecast"][u"forecastday"][1]
     
     def get_forecast_next_step_string(self):
         s = self.get_forecast_next_step()
-        return "{} you should expect {}".format(s['title'], s['fcttext'])
+        return u"{} you should expect {}".format(s[u'title'], s[u'fcttext'])
     
     def get_forecast_two_steps(self):
-        return self.fore_json["forecast"]["txt_forecast"]["forecastday"][2]
+        return self.fore_json[u"forecast"][u"txt_forecast"][u"forecastday"][2]
         
     def get_forecast_two_steps_string(self):
         s = self.get_forecast_two_steps()
-        return "{} you should expect {}".format(s['title'], s['fcttext'])
+        return u"{} you should expect {}".format(s[u'title'], s[u'fcttext'])
     
     def get_report_string(self):
-        s = "{} {} {}".format(self.get_conditions_string(), self.get_forecast_next_step_string(), self.get_forecast_two_steps_string())
+        s = u"{} {} {}".format(self.get_conditions_string(), self.get_forecast_next_step_string(), self.get_forecast_two_steps_string())
         return self.format_string(s)
 
     def print_curr_json(self):
-        print(json.dumps(self.curr_json, sort_keys = True, indent = 4))
+        print json.dumps(self.curr_json, sort_keys = True, indent = 4)
     
     def print_fore_json(self):
-        print(json.dumps(self.fore_json, sort_keys = True, indent = 4))
+        print json.dumps(self.fore_json, sort_keys = True, indent = 4)
         
     def read_mp3(self):
         vlc.MediaPlayer(self.mp3_filename).play()
         
     def write_report_string(self):
-        f = open('weather.txt', 'w')
+        f = open(u'weather.txt', u'w')
         f.write(self.get_report_string())
         f.close()
         
 def main():
-    key = WeatherReporter('WeatherUndergroundAPIKey', 'PA', 'New_Wilmington')
-    key.write_report_string()
+    key = WeatherReporter(u'WeatherUndergroundAPIKey', u'PA', u'New_Wilmington')
     key.get_report_mp3()
     key.read_mp3()
     
     
-if __name__ == "__main__" :
+if __name__ == u"__main__" :
     main()
